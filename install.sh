b9c58fb844037ceefb6b47006df13dfdf7df9b1c
yukio.goto <yukio.goto@mail.rakuten.com>
2012-05-01 17:59:03 +0900

    fix error to copy man dir

--- install.sh
+++ install.sh
@@ -11,15 +11,18 @@ fi
 echo "Installing php-build in $PREFIX"
 
 BIN_DIR="$PREFIX/bin"
-MAN_DIR="$PREFIX/share/man1"
+MAN1_DIR="$PREFIX/share/man/man1"
+MAN5_DIR="$PREFIX/share/man/man5"
 TMP_DIR="/tmp/php-build"
 
 echo -n "  - Creating Directories..."
 
 [ ! -d "$BIN_DIR" ]          && mkdir -p "$BIN_DIR"
-[ ! -d "$MAN_DIR" ]          && mkdir -p "$MAN_DIR"
+[ ! -d "$MAN1_DIR" ]          && mkdir -p "$MAN1_DIR"
+[ ! -d "$MAN5_DIR" ]          && mkdir -p "$MAN5_DIR"
 [ ! -d "$TMP_DIR/packages" ] && mkdir -p "$TMP_DIR/packages"
 [ ! -d "$TMP_DIR/source" ]   && mkdir -p "$TMP_DIR/source"
+
 
 echo " Done."
 
